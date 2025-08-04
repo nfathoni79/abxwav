dac426561c3e225e341be3809c67e48a86aebea6
NN Fathoni <naufalfathoni79@gmail.com>
2025-08-04 13:46:53 +0700

    Add sound icon, result text

--- src/components/ARadio.vue
+++ src/components/ARadio.vue
@@ -9,7 +9,8 @@ const model = defineModel()
 </script>
 
 <template>
-  <label :for="props.id" class="border border-gray-900 rounded-lg
+  <label :for="props.id" class="inline-flex justify-center items-center
+    border border-gray-900 rounded-lg
     bg-white hover:bg-gray-900 has-[:checked]:bg-gray-900
     px-4 py-2 text-gray-900 hover:text-white has-[:checked]:text-white">
     
